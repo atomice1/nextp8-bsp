d9c37f30a244df7c9106fc64f3201e9422067188
Chris January <chris@atomice.net>
2026-08-13 18:30:55 +0100

    temp diag for exit issue

--- src/exit.c
+++ src/exit.c
@@ -25,33 +25,48 @@ char *last_error = NULL;
 
 void __attribute__ ((noreturn)) _exit (int code)
 {
+  _set_postcode(9);
   if (code != 0)
     {
-      if (last_error)
+      _set_postcode(10);
+      if (last_error) {
+        _set_postcode(11);
         _fatal_error(last_error);
-      else
+      }
+      else {
+        _set_postcode(12);
         _fatal_error("fatal error");
+      }
     }
   else
     {
 #ifdef ROM
       if (_config_data && _config_data->exit_action == 1)
         {
+          _set_postcode(13);
           _shutdown();
+          _set_postcode(14);
         }
       else
         {
+          _set_postcode(15);
           _warm_reset();
+          _set_postcode(16);
         }
 #else
       if (_config_data && _config_data->exit_action == 1)
         {
+          _set_postcode(17);
           _shutdown();
+          _set_postcode(118);
         }
       else
         {
+          _set_postcode(19);
           _restart();
+          _set_postcode(20);
         }
 #endif
     }
+    _set_postcode(21);
 }
